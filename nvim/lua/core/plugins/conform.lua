4292d04ecd3c98ceac7c396a0b5da692f02217cd
Marcus Hägglund <mhagglun@gmail.com>
2024-11-25 19:11:12 +0100

    configure json formatting

--- nvim/lua/core/plugins/conform.lua
+++ nvim/lua/core/plugins/conform.lua
@@ -7,6 +7,7 @@ return {
                 formatters_by_ft = {
                     lua = { "stylua" },
                     python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
+                    json = { "prettierd", "prettier", stop_after_first = true },
                     javascript = { "prettierd", "prettier", stop_after_first = true },
                     typescript = { "prettierd", "prettier", stop_after_first = true },
                     typescriptreact = { "prettierd", "prettier", stop_after_first = true },
