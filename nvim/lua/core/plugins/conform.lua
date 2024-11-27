20745f6d3dcdb8ca0a0f7d08df777b8ffc7fb77c
Marcus Hägglund <mhagglun@gmail.com>
2024-11-27 22:15:03 +0100

    add yaml fmt

--- nvim/lua/core/plugins/conform.lua
+++ nvim/lua/core/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
                     lua = { "stylua" },
                     python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
                     json = { "prettierd", "prettier", stop_after_first = true },
+                    yaml = { "yamlfmt" },
                     javascript = { "prettierd", "prettier", stop_after_first = true },
                     typescript = { "prettierd", "prettier", stop_after_first = true },
                     typescriptreact = { "prettierd", "prettier", stop_after_first = true },
