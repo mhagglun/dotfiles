645256f71e77b219bc5ff1bb5ae67ea141b597eb
Marcus Hägglund <mhagglun@gmail.com>
2024-12-19 18:17:22 +0100

    fzf lsp code actions

--- nvim/lua/core/plugins/lspconfig.lua
+++ nvim/lua/core/plugins/lspconfig.lua
@@ -73,7 +73,7 @@ return {
                             capabilities = pyright_capabilities,
                             settings = {
                                 basedpyright = {
-                                    -- Using Ruff's import organizer
+                                    -- Using Ruff"s import organizer
                                     disableOrganizeImports = true,
                                     analysis = {
                                         typeCheckingMode = "basic", -- off, basic, standard, strict, all
@@ -106,7 +106,7 @@ return {
                             settings = {
                                 typescript = {
                                     inlayHints = {
-                                        includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+                                        includeInlayParameterNameHints = "all", -- "none" | "literals" | "all"
                                         includeInlayParameterNameHintsWhenArgumentMatchesName = false,
                                         includeInlayFunctionParameterTypeHints = true,
                                         includeInlayVariableTypeHints = false,
@@ -184,28 +184,38 @@ return {
                 }
             })
 
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('user_lsp_attach', { clear = true }),
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("user_lsp_attach", { clear = true }),
                 callback = function(event)
                     local client = vim.lsp.get_client_by_id(event.data.client_id)
                     -- Disabled due to https://github.com/neovim/neovim/issues/23164
                     client.server_capabilities.semanticTokensProvider = nil
 
                     local opts = { buffer = event.buf }
-                    vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
-                    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-                    vim.keymap.set('n', 'grr', function() vim.lsp.buf.references() end, opts)
-                    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-                    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-                    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-                    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
-                    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
-                    vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
-                    vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
-                    vim.keymap.set('n', '<leader>hs', function() vim.lsp.buf.signature_help() end, opts)
+                    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+                    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set("n", "grr", function() vim.lsp.buf.references() end, opts)
+                    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+                    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+                    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+                    vim.keymap.set("n", "<leader>hs", function() vim.lsp.buf.signature_help() end, opts)
+                    vim.keymap.set("n", "<leader>ca", function()
+                        require("fzf-lua").lsp_code_actions {
+                            winopts = {
+                                relative = "cursor",
+                                height = 0.33,
+                                width = 0.33,
+                                row = 1,
+                                preview = { horizontal = "right:40%" },
+                            },
+                        }
+                    end, { desc = "Code action" })
 
                     -- Toggle inlay hints
-                    vim.keymap.set('n', '<leader>th',
+                    vim.keymap.set("n", "<leader>th",
                         function()
                             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(), { 0 })
                         end, opts)
