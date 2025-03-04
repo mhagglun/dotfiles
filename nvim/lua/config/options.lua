63c2dcbbe59427be09f8ce9f5cc11ba8ce5b8fb3
Marcus Hägglund <mhagglun@gmail.com>
2025-03-04 21:43:04 +0100

    default tabstop=2

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,9 +7,9 @@ opt.relativenumber = true
 opt.clipboard = "unnamedplus"
 opt.spelllang = { "en_us" }
 
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.smartindent = true
 
