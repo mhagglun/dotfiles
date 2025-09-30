a5ec639600b8990ef3f6e8294b07f06ac8cc4dff
Marcus Hägglund <mhagglun@gmail.com>
2025-09-30 16:20:09 +0200

    fzf grep hidden files

--- nvim/lua/plugins/configs/fzf.lua
+++ nvim/lua/plugins/configs/fzf.lua
@@ -1,6 +1,9 @@
 local fzf = require("fzf-lua")
 local actions = fzf.actions
 fzf.setup({
+  grep = {
+    rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case -g '!{.git,.venv,node_modules}/*'"
+  },
   winopts = {
     border = "rounded",
   },
